98aaaa188430729324ff85e6609fa6062b9563ab
Rafael Ferreira <raffaferr@gmail.com>
2024-06-04 10:20:13 -0300

    setup prisma

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import { app } from "./app";
+import { app } from './app'
 
-
-app.listen({
-  host:'0.0.0.0',
-  port:3333
-}).then(()=>{
-  console.log('HTTP Server Running')
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running')
+  })
